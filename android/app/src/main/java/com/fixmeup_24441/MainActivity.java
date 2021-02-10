7e220098a1fbe40041be06d38c3e0ed7da80df73
Crowdbotics <team@crowdbotics.com>
2021-02-10 23:23:20 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/fixmeup_24441/MainActivity.java
@@ -0,0 +1,15 @@
+package com.fixmeup_24441;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "fixmeup_24441";
+    }
+}
---
+++ backend/fixmeup_24441/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for fixmeup_24441 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fixmeup_24441.settings')
+
+application = get_wsgi_application()
